04ef506940944fe3c245e7d0e8044965f4824627
as59623 <45113687+as59623@users.noreply.github.com>
2018-12-03 11:55:06 +0800

    Add files via upload

---
+++ marble/js/part3.js
@@ -0,0 +1,74 @@
+$(function(){
+        })
+        function openDialogx1(){
+            document.getElementById('x1').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogx1(){
+            document.getElementById('x1').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogx2(){
+            document.getElementById('x2').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogx2(){
+            document.getElementById('x2').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogx3(){
+            document.getElementById('x3').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogx3(){
+            document.getElementById('x3').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogx4(){
+            document.getElementById('x4').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogx4(){
+            document.getElementById('x4').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogx5(){
+            document.getElementById('x5').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogx5(){
+            document.getElementById('x5').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogx6(){
+            document.getElementById('x6').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogx6(){
+            document.getElementById('x6').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogx7(){
+            document.getElementById('x7').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogx7(){
+            document.getElementById('x7').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogx8(){
+            document.getElementById('x8').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogx8(){
+            document.getElementById('x8').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogx9(){
+            document.getElementById('x9').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogx9(){
+            document.getElementById('x9').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
