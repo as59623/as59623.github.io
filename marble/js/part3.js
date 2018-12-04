fe4aa89b6ba34a98d5fb06904d29da3850087afd
as59623 <as59623@gmail.com>
2018-12-05 01:19:37 +0800

    update2

--- marble/js/part3.js
+++ marble/js/part3.js
@@ -72,3 +72,91 @@ $(function(){
             document.getElementById('x9').style.display='none';
             document.getElementById('fade').style.display='none'
         }
+        function openDialogy1(){
+            document.getElementById('y1').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogy1(){
+            document.getElementById('y1').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogy2(){
+            document.getElementById('y2').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogy2(){
+            document.getElementById('y2').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogy3(){
+            document.getElementById('y3').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogy3(){
+            document.getElementById('y3').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }        
+        function openDialogy4(){
+            document.getElementById('y4').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogy4(){
+            document.getElementById('y4').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }        
+        function openDialogy5(){
+            document.getElementById('y5').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogy5(){
+            document.getElementById('y5').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogy6(){
+            document.getElementById('y6').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogy6(){
+            document.getElementById('y6').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogy7(){
+            document.getElementById('y7').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogy7(){
+            document.getElementById('y7').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogy8(){
+            document.getElementById('y8').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogy8(){
+            document.getElementById('y8').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogy9(){
+            document.getElementById('y9').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogy9(){
+            document.getElementById('y9').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogz1(){
+            document.getElementById('z1').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogz1(){
+            document.getElementById('z1').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
+        function openDialogz2(){
+            document.getElementById('z2').style.display='block';
+            document.getElementById('fade').style.display='block'
+        }
+        function closeDialogz2(){
+            document.getElementById('z2').style.display='none';
+            document.getElementById('fade').style.display='none'
+        }
